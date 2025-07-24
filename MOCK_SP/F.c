40eea18ff84feb7b8af450aae73f5c8537fd5b24
glutonium <146271709+glutonium404@users.noreply.github.com>
2025-07-24 14:29:17 +0600

    Update F.c

--- MOCK_SP/F.c
+++ MOCK_SP/F.c
@@ -4,23 +4,13 @@ int main() {
    int T;
    scanf("%d", &T);
 
-   int inputs[T][3];
+   int inputs[T];
 
    for(int i=0; i<T; i++) {
-      for (int j = 0; j < 3; j++) {
-         scanf("%d", &inputs[i][j]);
-      }
+      scanf("%d", &inputs[i]);
    }
 
    for(int i=0; i<T; i++) {
-      int digit1 = inputs[i][0];
-      int digit2 = inputs[i][1];
-      int digit3 = inputs[i][2];
-
-      if(digit1 == 7 || digit2 == 7 || digit3 == 7) {
-         printf("YES\n");
-      }else {
-         printf("NO\n");
-      }
+      printf("%d\n", inputs[i] * inputs[i]);
    }
 }
